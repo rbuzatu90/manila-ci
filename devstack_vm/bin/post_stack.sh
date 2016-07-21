84d2ba240041d1a69bcae20470463353d5492286
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2016-07-21 20:39:14 +0300

    Remove net/subnet quota and raise number of max ips per port

--- devstack_vm/bin/post_stack.sh
+++ devstack_vm/bin/post_stack.sh
@@ -3,6 +3,7 @@
 set -x
 
 nova quota-class-update --instances 50 --cores 100 --ram $((51200*4)) --floating-ips 50 --security-groups 50 --security-group-rules 100 default
+neutron quota-update --subnet -1 --network -1 default
 # manila quota-class-update --shares 50 --share-networks 10 --gigabytes 2000 default
 # ERROR: 'Namespace' object has no attribute 'class_name'
 # Manila quota cannot be modified using API
