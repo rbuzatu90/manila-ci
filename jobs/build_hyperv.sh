ab6a63ebed8a0a6919a05a684f2d5bb1ce0a10a2
Remus Buzatu <r.buzatu90@gmail.com>
2016-08-04 16:43:12 +0300

    Added threaded jobs support
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

---
+++ jobs/build_hyperv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+hyperv_node=$1
+# Loading all the needed functions
+source /usr/local/src/manila-ci/jobs/utils.sh
+
+# Loading parameters
+source /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.txt
+
+# building HyperV node
+echo $hyperv_node
+join_hyperv $WIN_USER $WIN_PASS $hyperv_node 
--- jobs/run_devstack.sh
+++ jobs/run_devstack.sh
@@ -6,24 +6,53 @@ source /home/jenkins-slave/tools/keystonerc_admin
 # Loading functions
 source /usr/local/src/manila-ci/jobs/utils.sh
 
+# Building devstack as a threaded job
+echo `date -u +%H:%M:%S` "Started to build devstack as a threaded job"
+nohup /usr/local/src/manila-ci/jobs/build_devstack.sh > /home/jenkins-slave/logs/devstack-build-log-$ZUUL_UUID 2>&1 &
+pid_devstack=$!
 
-# Run devstack
-echo "Run stack.sh on devstack"
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run_devstack.sh" 6
-if [ $? -ne 0 ]
+# Building and joining HyperV nodes
+echo `date -u +%H:%M:%S` "Started building & joining Hyper-V node: $hyperv_node"
+nohup /usr/local/src/manila-ci/jobs/build_hyperv.sh $hyperv_node > /home/jenkins-slave/logs/hyperv-build-log-$ZUUL_UUID-$hyperv_node 2>&1 &
+pid_hv=$!
+
+TIME_COUNT=0
+PROC_COUNT=3
+
+echo `date -u +%H:%M:%S` "Start waiting for parallel init jobs."
+
+finished_devstack=0;
+finished_hv=0;
+while [[ $TIME_COUNT -lt 60 ]] && [[ $PROC_COUNT -gt 0 ]]; do
+    if [[ $finished_devstack -eq 0 ]]; then
+        ps -p $pid_devstack > /dev/null 2>&1 || finished_devstack=$?
+        [[ $finished_devstack -ne 0 ]] && PROC_COUNT=$(( $PROC_COUNT - 1 )) && echo `date -u +%H:%M:%S` "Finished building devstack"
+    fi
+    if [[ $finished_hv -eq 0 ]]; then
+        ps -p $pid_hv > /dev/null 2>&1 || finished_hv=$?
+        [[ $finished_hv -ne 0 ]] && PROC_COUNT=$(( $PROC_COUNT - 1 )) && echo `date -u +%H:%M:%S` "Finished building $hyperv_node"
+    fi
+    if [[ $PROC_COUNT -gt 0 ]]; then
+        sleep 1m
+        TIME_COUNT=$(( $TIME_COUNT +1 ))
+    fi
+done
+
+echo `date -u +%H:%M:%S` "Finished waiting for the parallel init jobs."
+echo `date -u +%H:%M:%S` "We looped $TIME_COUNT times, and when finishing we have $PROC_COUNT threads still active"
+
+OSTACK_PROJECT=`echo "$ZUUL_PROJECT" | cut -d/ -f2`
+
+if [[ ! -z $IS_DEBUG_JOB ]] && [[ $IS_DEBUG_JOB == "yes" ]]
     then
-    echo "Failed to install devstack on cinder vm!"
-    exit 1
-fi
-# Run post_stack
-echo "Run post_stack scripts on devstack"
-run_ssh_cmd_with_retry ubuntu@$DEVSTACK_FLOATING_IP $DEVSTACK_SSH_KEY "source /home/ubuntu/keystonerc && /home/ubuntu/bin/post_stack.sh" 6
-if [ $? -ne 0 ]
-then
-    echo "Failed post_stack!"
-    exit 1
+        echo "All build logs can be found in http://64.119.130.115/debug/$OSTACK_PROJECT/$ZUUL_CHANGE/$ZUUL_PATCHSET/$JOB_TYPE/"
+    else
+        echo "All build logs can be found in http://64.119.130.115/$OSTACK_PROJECT/$ZUUL_CHANGE/$ZUUL_PATCHSET/$JOB_TYPE/"
 fi
 
-echo "Joining Hyper-V node $hyperv_node"
-
-join_hyperv $WIN_USER $WIN_PASS $hyperv_node
+if [[ $PROC_COUNT -gt 0 ]]; then
+    kill -9 $pid_devstack > /dev/null 2>&1
+    kill -9 $pid_hv > /dev/null 2>&1
+    echo "Not all build threads finished in time, initialization process failed."
+    exit 1
+fi
