ae485294461cc3892e497e0c8c901d2b92f12043
Remus Buzatu <r.buzatu90@gmail.com>
2017-02-17 00:20:56 +0200

    Added stable/ocata to branch check for create cells

--- jobs/handled_share_servers/initialize.sh
+++ jobs/handled_share_servers/initialize.sh
@@ -15,7 +15,7 @@ source /home/jenkins-slave/runs/devstack_params.$ZUUL_UUID.manila.txt
 run_ssh_cmd_with_retry ubuntu@$FIXED_IP $DEVSTACK_SSH_KEY \
     'source /home/ubuntu/keystonerc; NOVA_COUNT=$(nova service-list | grep nova-compute | grep -c -w up); if [ "$NOVA_COUNT" != 1 ];then nova service-list; exit 1;fi' 12 
 
-if [ "$ZUUL_BRANCH" == "master" ]; then
+if [[ "$ZUUL_BRANCH" == "master" ]] || [[ "$ZUUL_BRANCH" == "stable/ocata" ]]; then
     run_ssh_cmd_with_retry ubuntu@$FIXED_IP $DEVSTACK_SSH_KEY \
         "url=\$(grep transport_url /etc/nova/nova-dhcpbridge.conf | awk '{print \$3}'); nova-manage cell_v2 simple_cell_setup --transport-url \$url >> /opt/stack/logs/screen/create_cell.log"
 fi
